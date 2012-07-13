07a7fb500b29fb904fa85612444a7d80816b5a5f
Ben Bliss <ben.bliss@gmail.com>
2012-07-13 14:44:53 -0500

    Added caching for announcements.

--- megaphone/tasks.py
+++ megaphone/tasks.py
@@ -4,6 +4,7 @@ import json
 
 from django.contrib.sites.models import Site
 from django.db.models.loading import get_model
+from django.core.cache import cache
 
 from celery.decorators import task
 
@@ -15,9 +16,9 @@ def send_announcement(announcement_id):
     channels = []
     for site in announcement.sites.all():
         channels.append('megaphone' + str(site.id))
+        cache.set(str(site.id) + 'Megaphone', announcement.main_text, 60 * 60)
 
     msg = {'channels': channels, 'data': [announcement.main_text, announcement.pub_date]}
     server = redis.Redis()
     dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime) else None
     server.publish('juggernaut', json.dumps(msg, default=dthandler))
-
